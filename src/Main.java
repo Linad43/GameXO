39e1c1b55ae641856768cec9788c1dbf5c874d87
Linad <merzlikindaniil97@gmail.com>
2024-02-23 05:57:29 +0300

    Done GameXO

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,12 @@ public class Main
     }
     public static void plotXY ()
     {
-        System.out.println("#-1-2-3-");
+        System.out.print("#");
+        for (int i = 0; i < sizeX; i++) {
+            System.out.print("-" + (i+1));
+        }
+        System.out.println("-");
+
         for (int i = 0; i < sizeX; i++)
         {
             System.out.print(i+1);
@@ -34,7 +39,10 @@ public class Main
             }
             System.out.print("|\n");
         }
-        System.out.println("--------");
+        for (int i = 0; i < sizeX*2+2; i++) {
+            System.out.print("-");
+        }
+        System.out.print("\n");
     }
     public static void stepHum()
     {
@@ -47,9 +55,10 @@ public class Main
             }
             else
                 System.out.println("Введены не корректные данные повторите ввод: ");
-            x=scanner.nextInt();
-            y=scanner.nextInt();
+            x=scanner.nextInt()-1;
+            y=scanner.nextInt()-1;
         }while (!isCellValid(x,y) || !isCellEmpty(x,y));
+        field[x][y] = DOT_HUMAN;
     }
     public static void stepCpu()
     {
@@ -58,6 +67,7 @@ public class Main
             x=random.nextInt(sizeX);
             y=random.nextInt(sizeY);
         }while (!isCellEmpty(x,y));
+        field[x][y] = DOT_AI;
     }
     /**
      * Проверка, является ли ячейка игрового поля пустой
@@ -86,9 +96,119 @@ public class Main
      * @param s победный слоган
      * @return состояние игры
      */
+
+    public static boolean checkWin(char dot)
+    {
+        for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                if (field[i][j] == dot)
+                {
+                    return check1(i,j,dot) || check2(i,j,dot) || check3(i,j,dot) || check4(i,j,dot);
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean check1(int i, int j, char dot)
+    {
+        if(j+vin_count <= sizeY) {
+            for (int k = 0; k < vin_count; k++) {
+                if (field[i][j + k] != dot) {
+                    break;
+                }
+                if (k == vin_count - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean check2(int i, int j, char dot)
+    {
+        if(i+vin_count <= sizeX) {
+            for (int k = 0; k < vin_count; k++) {
+                if (field[i + k][j] != dot) {
+                    break;
+                }
+                if (k == vin_count - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean check3(int i, int j, char dot)
+    {
+        if(i+vin_count <= sizeX && j+vin_count <= sizeY) {
+            for (int k = 0; k < vin_count; k++) {
+                if (field[i + k][j + k] != dot) {
+                    break;
+                }
+                if (k == vin_count - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean check4(int i, int j, char dot)
+    {
+        if(i-vin_count <= sizeX && i-vin_count >= 0 && j+vin_count <= sizeY) {
+            for (int k = 0; k < vin_count; k++) {
+                if (field[i - k][j + k] != dot) {
+                    break;
+                }
+                if (k == vin_count - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    static boolean checkDraw(){
+        for (int x = 0; x < sizeX; x++){
+            for (int y = 0; y < sizeY; y++){
+                if (isCellEmpty(x, y))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    static boolean checkState(char dot, String s){
+        if (checkWin(dot)){
+            System.out.println(s);
+            return true;
+        }
+        else if (checkDraw()){
+            System.out.println("Ничья!");
+            return true;
+        }
+        // Игра продолжается
+        return false;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        begin();
-        plotXY();
+        while (true) {
+            begin();
+            plotXY();
+            while (true) {
+                stepHum();
+                plotXY();
+                if (checkState(DOT_HUMAN, "Вы победили!"))
+                    break;
+                stepCpu();
+                plotXY();
+                if (checkState(DOT_AI, "Компьютер победил!"))
+                    break;
+            }
+            System.out.print("Желаете сыграть еще раз? (Y - да): ");
+            if(!scanner.next().equalsIgnoreCase("Y"))
+                break;
+        }
     }
 }
